4e379a4d6a6b68ed979fbcd3d0b00c6f8693be9b
Michael Nowell <michaelnowell84@yahoo.com>
2024-04-05 19:41:05 +0800

    Add files via upload

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','ucdavis.edu','indiana.edu','berkeley.edu','ufp.pt','uct.ac.za','columbia.edu','etudes.org','gmail.com','virginia.edu'],
+['2005-12',57,11,12,12,10,14,13,5,10,9],
+['2006-01',103,34,30,30,31,26,25,27,19,17]
+];
